ac8ab5e7727964ace7b259f3264793a356286d2f
Christian Smith <cgr.gonzalezrossier@gmail.com>
2020-06-10 12:33:58 -0400

    Se agrega el NodoLista

---
+++ Prueba1.java
@@ -0,0 +1,14 @@
+class NodoLista {
+  int dato;
+  NodoLista siguiente;
+
+  NodoLista() {
+    dato = 0;
+    siguiente = null;
+  }
+
+  NodoLista(int x) {
+    dato = x;
+    siguiente = null;
+  }
+}
